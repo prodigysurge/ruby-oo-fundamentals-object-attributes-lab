e3cbb39e077793b0bc73fd10ab068bd9210556b8
prodigysurge <boatinglife6@yahoo.com>
2021-01-18 16:27:06 -0500

    commit tree 7edf07b7dd4dfb3e516deeaaa3af5829b8ae31ec

---
+++ lib/person.rb
@@ -0,0 +1,37 @@
+
+# class Dog
+#     def dog(name, breed)
+#         @name = name
+#         @breed = breed
+#     end
+#     def name=(name)
+#         @name = name
+#     end
+#     def breed=(breed)
+#         @breed = breed
+#     end
+#     def name
+#         @name
+#     end
+#     def breed
+#         @breed
+#     end
+# end
+class Person
+    def person(name, job)
+        @name = name
+        @job = job
+    end
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job
+    end
+    def name
+        @name
+    end
+    def job
+        @job
+    end
+end
